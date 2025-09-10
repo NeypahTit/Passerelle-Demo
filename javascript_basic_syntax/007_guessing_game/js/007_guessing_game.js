a058adb159c44a4a601594fad04a33d88963b75b
NeypahTit <www.wia343@gmail.com>
2025-09-10 10:59:44 +0200

    updated the guessing game a bit

--- javascript_basic_syntax/007_guessing_game/js/007_guessing_game.js
+++ javascript_basic_syntax/007_guessing_game/js/007_guessing_game.js
@@ -1,5 +1,7 @@
 //range is 0 ~ 10
 let rngCpuNum = Math.floor(Math.random() * 10);
+let min = 0;
+let max = 10;
 
 alert("Hello! I will think of a number between 0 and 10, and you must guess it within 5 attempts!");
 
@@ -12,13 +14,18 @@ for (i = 0; i <= 5; i++) {
 
     let userGuess = prompt("Please put in your guess! (Between 0 and 10) | Attempt: " + (i + 1));
 
-    if (userGuess < rngCpuNum) {
-        alert("Your guess is smaller than my number!");
-    } else if (userGuess > rngCpuNum) {
-        alert("Your guess is larger than my number!");
-    } else {
+    if (userGuess == rngCpuNum) {
         alert("Congratulations! You have guessed the number!"
             + "\nAmount of attempts: " + (i + 1));
         break;
+    } else {
+        if (userGuess > max || userGuess < min) {
+            alert("Your guess is outside of the boundary!");
+            i--;
+        } else if (userGuess < rngCpuNum) {
+            alert("Your guess is smaller than my number!");
+        } else {
+            alert("Your guess is larger than my number!");
+        }
     }
 }
