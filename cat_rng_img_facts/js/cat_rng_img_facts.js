b78a89cfe9848b19f499d94a2d9bfe149b79fc3c
NeypahTit <www.wia343@gmail.com>
2025-10-02 13:01:54 +0200

    made cat image getting an async function

--- cat_rng_img_facts/js/cat_rng_img_facts.js
+++ cat_rng_img_facts/js/cat_rng_img_facts.js
@@ -1,16 +1,32 @@
-/*
-TODO
-Make API fetching use `async`, to be able to catch errors in response
-*/
-
-this.onload = () => {
-    const catImageWidth = 800;
-    const catImageHeight = 600;
+this.onload = async () => {
+    const apiURL = "https://cataas.com";
     const catImageAppender = document.querySelector("#cat_image_appender");
     
-    let rngCatImageURL = `https://cataas.com/cat?width=${catImageWidth}&height=${catImageHeight}`;
     let rngCatImage = document.createElement("img");
 
-    rngCatImage.src = rngCatImageURL;
-    catImageAppender.append(rngCatImage);
+    getCatImage().then((img) => {
+        rngCatImage.src = img;
+        console.log(rngCatImage.src, "aaaa");
+        catImageAppender.append(rngCatImage);
+    }).catch((whyRejected) => {
+        console.warn("CAT IMAGE:", whyRejected);
+        alert("CAT IMAGE:", whyRejected);
+    });
+
+    /** 
+     * Get a random cat image using `https://cataas.com/cat` (API)
+     */
+    async function getCatImage() {
+        // Set the width and height
+        const catImageWidth = 800;
+        const catImageHeight = 600;
+        
+        // Set the url (with width and height), get its response, set the data (as response's url)
+        const rngCatImageURL = `${apiURL}/cat?width=${catImageWidth}&height=${catImageHeight}`;
+        const response = await fetch(rngCatImageURL);
+        const data = response.url;
+        
+        // Returns the url to use
+        return data;
+    }
 }
