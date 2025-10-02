0a3a4888d5d96b0a2f90c88df5646c75002bf023
NeypahTit <www.wia343@gmail.com>
2025-10-02 11:46:45 +0200

    tiny changes to random cat img/fact project

--- cat_rng_img_facts/js/cat_rng_img_facts.js
+++ cat_rng_img_facts/js/cat_rng_img_facts.js
@@ -3,12 +3,12 @@ TODO
 Make API fetching use `async`, to be able to catch errors in response
 */
 
-const catImageWidth = 800;
-const catImageHeight = 600;
-const rngCatImageURL = `https://cataas.com/cat?width=${catImageWidth}&height=${catImageHeight}`;
-
 this.onload = () => {
+    const catImageWidth = 800;
+    const catImageHeight = 600;
     const catImageAppender = document.querySelector("#cat_image_appender");
+    
+    let rngCatImageURL = `https://cataas.com/cat?width=${catImageWidth}&height=${catImageHeight}`;
     let rngCatImage = document.createElement("img");
 
     rngCatImage.src = rngCatImageURL;
